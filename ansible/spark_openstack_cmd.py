dd2c98093064dc9ae03998fb609c572c0b89e46a
Side Li <liside@users.noreply.github.com>
2020-01-08 15:30:04 -0800

    Update spark_openstack_cmd.py

--- ansible/spark_openstack_cmd.py
+++ ansible/spark_openstack_cmd.py
@@ -16,6 +16,7 @@ import urlparse
 
 spark_versions = \
     {
+        "2.4.4": {"hadoop_versions": ["2.7"]},
         "2.3.0": {"hadoop_versions": ["2.6", "2.7"]},
         "2.2.1": {"hadoop_versions": ["2.6", "2.7"]},
         "2.2.0": {"hadoop_versions": ["2.6", "2.7"]},
